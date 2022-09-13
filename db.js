90f2d248330baab714bf156ac877001442834623
CLOVIS SANTIAGO CAVALHEIRO <47414622+cscsenior@users.noreply.github.com>
2022-09-13 12:27:04 +0100

    Update db.js with functions CRUD

--- db.js
+++ db.js
@@ -1,3 +1,4 @@
+const OjectID = require("mongodb").ObjectID;
 const mongoClient = require("mongodb").MongoClient;
 mongoClient.connect("mongodb://localhost:27017",
                     { useUnifiedTopology: true})
@@ -14,4 +15,21 @@ function findCustomers(){
                  .toArray();
 }
 
-module.exports = { findCustomers }
+function insertCustomer(customer){
+    return global.connection
+                  .collection("customers")
+                  .isertOne(customer);
+
+function updatetCustomer(id, customer){
+    const objectId = new ObjectID(id);
+    return global.connection
+                  .collection("customers")
+                  .updateOne({_id: objectId}, {$set: customer});
+  
+function deleteCustomer(id){
+    const objectId = new ObjectID(id);
+    return global.connection
+                  .collection("customers")
+                  .deleteOne({_id: objectId});
+
+module.exports = { findCustomers, insertCustomer, updateCustomer, deleteCustomer }
